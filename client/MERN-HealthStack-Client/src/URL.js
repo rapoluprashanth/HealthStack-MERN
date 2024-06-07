552180f3d10fad77c05a951da8242096c4c0b35b
rapoluprashanth <rapoluprashanth123@gmail.com>
2024-06-07 22:58:49 +0530

    added

--- client/MERN-HealthStack-Client/src/URL.js
+++ client/MERN-HealthStack-Client/src/URL.js
@@ -1,1 +1,2 @@
-export const hostURL = "http://localhost:5000/";
+// export const hostURL = "http://localhost:5000/";
+export const hostURL = "https://health-stack-mern-server.vercel.app/";
--- client/MERN-HealthStack-Client/src/utils/APIRoutes.js
+++ client/MERN-HealthStack-Client/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const hostURL = "http://localhost:5000";
+// export const hostURL = "http://localhost:5000";
+export const hostURL = "https://health-stack-mern-server.vercel.app";
 export const registerRoute = `${hostURL}/api/auth/register`;
 export const loginRoute = `${hostURL}/api/auth/login`;
 export const medicineProductsRoute = `${hostURL}/api/products/medicines`;
